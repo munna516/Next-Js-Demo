0526ca834dd907e220a91c25b9dda3a20df2c68d
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-23 20:26:14 +0600

    all blogs and blogs details done

---
+++ src/app/blogs/[id]/page.jsx
@@ -0,0 +1,18 @@
+const BlogDetails = async ({ params }) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const blogs = await response.json();
+  return (
+    <>
+      <div className="card  shadow-xl mt-20 p-5 border">
+        <div className="card-body">
+          <h2 className="text-center font-bold text-lg md:-text-xl lg:text-3xl">Title - {blogs.title}</h2>
+          <p className="text-lg text-gray-500 mt-5">Description : {blogs.body}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogDetails;
--- src/app/components/Blog.jsx
+++ src/app/components/Blog.jsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
+
 const Blog = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const allBlogs = await response.json();
- 
+
   return (
-    <div>
-      {allBlogs.map((blogs) => (
-        <li key={blogs.id}>{blogs.title}</li>
-      ))}
-    </div>
+    <>
+      <div>
+        <h1 className="text-4xl text-center my-10 font-bold">All Blog Post</h1>
+      </div>
+      <div className="flex flex-col gap-7 mb-10">
+        {allBlogs.map((blogs) => (
+          <Link href={`/blogs/${blogs.id}`} key={blogs.id}>
+            <li className="text-lg md:text-xl lg:text-2xl hover:underline hover:text-blue-500">{blogs.title}</li>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
